ecb1b1ed1284c994fdcb14beab07e407c98dd15c
Gerald Versluis <gerald@verslu.is>
2020-04-09 11:34:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SourceLinkEssentials
+ Demo, well. Two lines of code used to demonstrate the power of Source Link
---
+++ SourceLinkEssentials/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SourceLinkEssentials
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var fooLabel = new Xamarin.Forms.Label();
+
+            Xamarin.Essentials.Geocoding.GetLocationsAsync("1 Microsoft Way, Redmond");
+        }
+    }
+}
